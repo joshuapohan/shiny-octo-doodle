b95b2ecd61229158c99707658b07d3ead698a238
joshuapohan <joshua.pohan@paper.id>
2020-12-03 16:23:58 +0700

    update layout and content

--- src/component/app/app.js
+++ src/component/app/app.js
@@ -4,6 +4,7 @@ import './app.css';
 import travelImage from '../../asset/undraw_travelers_qlt1.svg';
 import growImage from '../../asset/undraw_romantic_getaway_k2mf.svg';
 import placeholder from '../../asset/placeholder.webm';
+import placeholderQr from '../../asset/placeholder_qr.png';
 
 
 function App(){
@@ -18,6 +19,9 @@ function App(){
                 </div>
             </div>
             <div className="app-background-box">
+                <div className="app-neon-text-shadow app-large-icon-wrapper">
+                    <i className="fas fa-heart app-large-icon"></i><br></br>
+                </div>
                 <div className="app-separator-bar">
                     <div className="app-separator-box">
                         Lorem ipsum dolor sit amext
@@ -34,21 +38,16 @@ function App(){
                     </div>
                 </div>
             </div>
-            <div>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                </div>
             <div className="app-background-box">
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <div className="app-neon-text-shadow app-large-icon-wrapper">
+                    <i class="fas fa-tree app-large-icon"></i><br></br>
+                </div>
                 <div className="app-separator-bar">
                     <div className="app-separator-box">
                         Lorem ipsum dolor sit amext
@@ -63,6 +62,36 @@ function App(){
                     <div className="app-lower-right-box grow-image-wrapper">
                         <img src={growImage} className="grow-image"></img>
                     </div>
+                </div>
+                <div className="app-qr-wrapper">
+                    <div className="app-qr">
+                        <span className="app-neon-text-shadow app-qr-text">
+                            Lorem Ipsum QR hahaha
+                        </span>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <img src={placeholderQr}></img>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+            </div>
+            <div className="app-background-box">
+                <div className="app-footer-content">
+                    Lorem Ipsum Footer Here Adress etc c
                 </div>
             </div>
         </div>
